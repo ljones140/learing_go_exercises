87c4ab7fe62d45f6a3dd4031d28971d686f0ff75
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2019-11-03 18:15:26 +0000

    Functions working on Rectangle type only

---
+++ structs_methods_interfaces/shapes_test.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"testing"
+)
+
+type Rectangle struct {
+	Width  float64
+	Height float64
+}
+
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(
+		Rectangle{10.0, 10.0},
+	)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
+
+func TestArea(t *testing.T) {
+	got := Area(
+		Rectangle{12.0, 6.0},
+	)
+	want := 72.0
+
+	if got != want {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
+}
