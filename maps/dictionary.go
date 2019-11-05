90ec1dd1f478674630ea9f5f25947cbece166ca9
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2019-11-05 22:11:45 +0000

    Search words with errors

--- maps/dictionary.go
+++ maps/dictionary.go
@@ -1,7 +1,17 @@
 package main
 
+import (
+	"errors"
+)
+
 type Dictionary map[string]string
 
-func (d Dictionary) Search(word string) string {
-	return d[word]
+var ErrNotFound = errors.New("Could not find supplied word")
+
+func (d Dictionary) Search(word string) (string, error) {
+	definition, ok := d[word]
+	if !ok {
+		return "", ErrNotFound
+	}
+	return definition, nil
 }
--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -5,16 +5,31 @@ import "testing"
 func TestSearch(t *testing.T) {
 	dictionary := Dictionary{"test": "this is just a test"}
 
-	got := dictionary.Search("test")
-	want := "this is just a test"
+	t.Run("word in dictionary", func(t *testing.T) {
 
-	assertStrings(t, got, want)
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+
+		_, err := dictionary.Search("mooky")
+		want := "Could not find supplied word"
+
+		if err == nil {
+			t.Fatal(ErrNotFound)
+		}
+
+		assertStrings(t, err.Error(), want)
+	})
 }
 
 func assertStrings(t *testing.T, got, want string) {
 	t.Helper()
 
 	if got != want {
-		t.Errorf("got %q want %q given, %q", got, want, "test")
+		t.Errorf("got %q want %q", got, want)
 	}
 }
