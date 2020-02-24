bea919d5f4b01fca1664b1580a05bee064c9612b
Lewis Jones <lewis.jones@simplybusiness.co.uk>
2020-02-24 22:00:41 +0000

    Extract value from maps

--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -65,6 +65,14 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Reykjavik"},
 		},
+		{
+			"Maps",
+			map[string]string{
+				"Foo": "Bar",
+				"Baz": "Boz",
+			},
+			[]string{"Bar", "Boz"},
+		},
 	}
 
 	for _, test := range cases {
